d3b14381712dcfb996f63c5c255f84ea4ce80589
je-lopez <je.lopez@me.com>
2017-05-18 12:34:59 -0700

    adds/corrects some functionality

--- public/index.js
+++ public/index.js
@@ -1,69 +1,140 @@
-var output = ["0"];
-// get all buttons and add a click event listener
+// TODO: I reverted back to not nesting everything to get it mostly working again. Am I too deep? tips?
+var output = [];
+// TODO: look into bubbling! (note to self)
 var buttons = document.getElementsByClassName( "calculator-btn" );
 
-for ( var i = 0; i < buttons.length; i++ ) {
+for (i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener( "click", function() {
     var value = this.getAttribute( "data-value" );
 
-    isNaN( value ) ? operators( value ) : numbers( value );
+    controller(value);
   })
-}
-
-function numbers( number ) {
-  // if last item in output is a number, concat, otherwise push it and call controller
-  if ( Number.isInteger( parseInt( output[output.length - 1] ) ) ) {
-    output.push( output.splice(-1) + number );
-  } else {
-    output.push( number );
-    controller();
-  }
-}
-
-function operators( operator ) {
-  switch( operator ) {
-    case "clear":
-      clear();
-      updateOutput( 0 );
-      break;
-    case "percent":
-      percentage();
-      break;
-  }
 }
 
 function clear() {
   output.length = 0;
-  output.push("0");
+  document.querySelector("#clear").innerText = "AC";
+  updateOutput(0);
 }
 
 function percentage() {
-  updateOutput( output[output.length - 1] / 100 );
-  clear();
+  return output.pop() / 100;
 }
 
-function updateOutput( value ) {
-  document.getElementById("calculator-output").innerText = value;
+function posNeg() {
+  return output.pop() * -1;
 }
 
-// function equals() {
-//   for ( var i = 0; i < output.length; i++ ) {
-//     parseInt( output[i] )
-//   }
-// }
+function dot() {
+  return output.pop() + ".";
+}
 
-function controller( value ) {
-  output[output.length - 2];
+// TODO: refactor mult/div since only difference is (* vs /)
+function multiply(position, value) {
+  return position === -1 ? output[output.length - 2] * value : output[0] * output[2];
+}
 
-  switch( value ) {
-    case "*":
-    case "/":
-      updateOutput();
-      break;
-    case "=":
-      equals();
-      break;
-    default:
-      output.push( value );
+function divide(position, value) {
+  return position === -1 ? output[output.length - 2] / value : output[0] / output[2];
+}
+
+function add() {
+  return parseInt(output[0]) + parseInt(output[2]);
+}
+
+function subtract() {
+  return parseInt(output[0]) - parseInt(output[0]);
+}
+
+function calculate(argument) {
+  // body...
+}
+
+function updateOutput(val) {
+  console.log(output);
+  document.querySelector("#calculator-output").innerText = val;
+}
+
+function controller(value) {
+  // TODO: add blink effect via fade in/fade out ?
+
+  if (output.length === 0) {
+    if (isNaN(value)) {
+      return;
+    } else {
+      document.querySelector("#clear").innerText = "C";
+    }
+  }
+
+// TODO: nest this switch in the 1000th if statement to remove duplication ? or better way to handle it?
+// TODO: number with "." isn't calculated properly
+  switch (value) {
+    case "clear":
+      clear();
+      return;
+    case "%":
+      if (isNaN(output[output.length - 1])) return;
+      var val = percentage();
+      output.push(val);
+      updateOutput(val);
+      return;
+    case "pos-neg":
+      if (isNaN(output[output.length - 1])) return;
+      var val = posNeg();
+      output.push(val);
+      updateOutput(val);
+      return;
+    case ".":
+      if (isNaN(output[output.length - 1])) return;
+      var val = dot();
+      output.push(val);
+      updateOutput(val);
+      return;
+    // default:
+    //   calculate();
+    //   return;
+  }
+
+  if (!isNaN(value) && !isNaN(output[output.length - 1])) {
+    output.push(output.pop() + value);
+    updateOutput(output[output.length - 1]);
+    return;
+  }
+
+  if (output.length < 3) {
+    output.push(value);
+    if (!isNaN(value)) updateOutput(value);
+    return;
+  }
+
+  if (output.length < 4) {
+    var val;
+
+    if (["*", "/"].includes(output[1])) {
+      val = output[1] === "*" ? multiply(0) : divide(0);
+      output = [val, value];
+      updateOutput(val);
+    } else if (!["*", "/"].includes(value)) {
+      val = output[1] === "+" ? add() : subtract();
+      output = [val, value];
+      updateOutput(val);
+    } else {
+      output.push(value);
+    }
+
+    return;
+  } else {
+    var val;
+
+    if (["*", "/"].includes(output[3])) {
+      val = output[3] === "*" ? multiply(-1, value) : divide(-1, value);
+      output = output.splice(0, 2);
+      output.push(val);
+    } else {
+      val = output[1] === "+" ? add() : subtract();
+      output = [val, value];
+    }
+
+    updateOutput(val);
   }
 }
